d2e29ca56157284a528bcbc2c21e8e1a15c57027
GoareguerLucas <lucas.goareguer@etu.univ-amu.fr>
2019-04-29 09:38:57 +0200

    BugFix

--- W4_112_dcgan/dcgan.py
+++ W4_112_dcgan/dcgan.py
@@ -24,8 +24,8 @@ import matplotlib.pyplot as plt
 import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=5000, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=16, help="size of the batches")
+parser.add_argument("--n_epochs", type=int, default=1000, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lrD", type=float, default=0.0004, help="adam: learning rate for D")
 parser.add_argument("--lrG", type=float, default=0.0004, help="adam: learning rate for G")
 parser.add_argument("--eps", type=float, default=0.00005, help="batchnorm: espilon for numerical stability")
