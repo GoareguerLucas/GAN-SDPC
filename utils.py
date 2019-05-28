1dcf6220d942e0b07d540de8c0ce492746b745e6
GoareguerLucas <lucas.goareguer@etu.univ-amu.fr>
2019-05-28 15:56:21 +0200

    BugFix

--- utils.py
+++ utils.py
@@ -188,7 +188,7 @@ def sampling(noise, generator, path, epoch, HSV=False):
 		batch = np.asarray(batch)
 		print(type(batch))
 		print(batch.shape)
-		gen_imgs = batch
+		gen_imgs = torch.from_numpy(batch)
 			
 	save_image(gen_imgs.data[:], "%s/%d.png" % (path, epoch), nrow=5, normalize=True)
 	generator.train()
