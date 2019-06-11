9c204b3fe9804237afd311f21bbf917b3236202c
GoareguerLucas <lucas.goareguer@etu.univ-amu.fr>
2019-06-11 14:42:25 +0200

    FixBug

--- W11_128treshold_dcgan/seuil=0.05/dcgan.py
+++ W11_128treshold_dcgan/seuil=0.05/dcgan.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--n_epochs", type=int, default=300, help="number of epochs of training")
+parser.add_argument("-e", "--n_epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("-b", "--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lrD", type=float, default=0.00004, help="adam: learning rate for D")
 parser.add_argument("--lrG", type=float, default=0.0004, help="adam: learning rate for G")
@@ -29,9 +29,9 @@ parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first 
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("-i", "--img_size", type=int, default=32, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument("-s", "--sample_interval", type=int, default=10, help="interval between image sampling")
+parser.add_argument("-i", "--img_size", type=int, default=128, help="size of each image dimension")
+parser.add_argument("--channels", type=int, default=3, help="number of image channels")
+parser.add_argument("-s", "--sample_interval", type=int, default=4, help="interval between image sampling")
 parser.add_argument("--sample_path", type=str, default='images')
 parser.add_argument("-m", "--model_save_interval", type=int, default=2500, help="interval between image sampling")
 parser.add_argument('--model_save_path', type=str, default='models')
