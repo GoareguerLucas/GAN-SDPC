01d071572f20962517acc31a5bd408c7e458a788
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-06-14 09:07:07 +0200

    launch script for the mesocentre

--- launch.sh
+++ launch.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #SBATCH -J Job_GAN # <-- Change that
-#SBATCH -p teslak20 # <-- Could be also teslak40 (less memory)
+#SBATCH -p gpu # <-- Could be also teslak40 (less memory)
 #SBATCH --gres=gpu:1 # <-- Specify that you want to use 1 gpu for your python prog
 #SBATCH -A h146  # <-- Do not change, we are all working under the same project
 #SBATCH -t 2-2  # <-- IMPORTANT :  this the duration of the simulation at the format : dd-hh:mm:ss. The simulation stopped even if not finished
 #SBATCH -N 1 # <-- The number of node you want to use
-#SBATCH -o ./test.out  # <-- the name of the file where the output of the simulation is written
-#SBATCH -e ./error.err  # <-- the name of the file where errors of the simulation are written
+#SBATCH -o test.out  # <-- the name of the file where the output of the simulation is written
+#SBATCH -e error.err  # <-- the name of the file where errors of the simulation are written
 
 module purge
 module load userspace/all
