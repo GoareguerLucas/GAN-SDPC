d63e823f52df150ab0ba7420f3f1b0c6c8738f9d
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-06-24 17:15:42 +0200

    launch 128_norm

--- launch.sh
+++ launch.sh
@@ -56,9 +56,13 @@ module load python3/3.6.3
 # python3 dcgan.py  # <-- Put here the name of the python prog you want to launch
 # cd ..
 
-cd W12_AE_dcgan_128_kernels
+cd W12_AE_dcgan_128_norm
 python3 dcgan.py  # <-- Put here the name of the python prog you want to launch
 cd ..
+
+# cd W12_AE_dcgan_128_kernels
+# python3 dcgan.py  # <-- Put here the name of the python prog you want to launch
+# cd ..
 
 # cd W12_AE_dcgan_64_leaky
 # python3 dcgan.py  # <-- Put here the name of the python prog you want to launch
