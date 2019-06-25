09d3cffc3eaabf972f770ee5762e7444f7076bda
GoareguerLucas <lucas.goareguer@etu.univ-amu.fr>
2019-06-25 16:54:16 +0200

    FixBug

--- W13_current_dcgan/dcgan.py
+++ W13_current_dcgan/dcgan.py
@@ -31,7 +31,6 @@ parser.add_argument("--lrG", type=float, default=0.0004, help="adam: learning ra
 parser.add_argument("--eps", type=float, default=0.00005, help="batchnorm: espilon for numerical stability")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
 parser.add_argument("-i", "--img_size", type=int, default=64, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
@@ -230,7 +229,7 @@ if opt.load_model == True:
 # Les runs sont sauvegarder dans un dossiers "runs" à la racine du projet, dans un sous dossiers opt.runs_path.
 os.makedirs(depth + "../runs/" + opt.runs_path, exist_ok=True)
 
-writer = SummaryWriter(log_dir=depth + "../runs/" + opt.runs_path)
+writer = SummaryWriter(log_dir=depth + "../runs/" + opt.runs_path + timetag)
 
 # ----------
 #  Training
