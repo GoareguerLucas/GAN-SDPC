2565ff997c56b2c21bd38ad3d55885328743a395
GoareguerLucas <lucas.goareguer@etu.univ-amu.fr>
2019-06-27 12:44:22 +0200

    Exp : LantentDim

---
+++ W13_latentDim_dcgan/scan_params.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.path.append("../")
+
+from utils import *
+
+"""
+Script à utiliser pour lancer un modèle avec différent paramètres à la suite, avec gestion des résultats dans tensorboard.
+Remplir la variables exp_name avec le nom de l'expérience et le dictionnaire params avec les paramètres à tester pour
+keys et leurs valeurs successive pour values.
+
+Pour scanner un nouveau paramètre il suffit d'ajouter l'argument correspondant dans sont dcgan.py. 
+Le script utiliser (dcgan.py) doit être compatible avec le paramètre --runs_path (-r) pour utiliser cette fonction 
+(cf. W13_current_dcgan/dcgan.py).
+Il ne faut pas ajouter au dictionnaire params l'argument --runs_path (-r), tout est gérer dans la fonction scan.
+"""
+
+exp_name = "LatentDim"
+
+# Dictionnaire des paramètres à tester avec : noms du "paramètre" : [liste des valeurs]
+params = {"--latent_dim": [50, 150, 200]}
+
+scan(exp_name, params)
