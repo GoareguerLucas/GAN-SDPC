497a9e01f1699a81ca4e3bc418373958242c5220
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-07-04 16:58:40 +0200

    using kepler

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH -J Job_GAN # <-- Change that
-#SBATCH -p gpu # <-- Could be also teslak40 (less memory)
+#SBATCH -p kepler # gpu # <-- Could be also teslak40 (less memory)
 #SBATCH --gres=gpu:1 # <-- Specify that you want to use 1 gpu for your python prog
 #SBATCH -A h146  # <-- Do not change, we are all working under the same project
 #SBATCH -t 2-2  # <-- IMPORTANT :  this the duration of the simulation at the format : dd-hh:mm:ss. The simulation stopped even if not finished
@@ -8,9 +8,9 @@
 #SBATCH -o test.out  # <-- the name of the file where the output of the simulation is written
 #SBATCH -e error.err  # <-- the name of the file where errors of the simulation are written
 
-module purge
-module load userspace/all
-module load python3/3.6.3
+# module purge
+# module load userspace/all
+# module load python3/3.6.3
 
 # cd W11_AE_dcgan_64
 # python3 dcgan.py  # <-- Put here the name of the python prog you want to launch
