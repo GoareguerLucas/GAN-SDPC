397a867537009ed8d61ccfcbfc88396dbe7eff52
GoareguerLucas <lucas.goareguer@etu.univ-amu.fr>
2019-07-08 16:22:50 +0200

    FixBug

--- W15_AE_dcgan_large/dcgan.py
+++ W15_AE_dcgan_large/dcgan.py
@@ -23,7 +23,7 @@ import time
 import datetime
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-r", "--runs_path", type=str, default='AE128Small/200e128i64b/',
+parser.add_argument("-r", "--runs_path", type=str, default='AE128Large/200e128i64b/',
                     help="Dossier de stockage des résultats sous la forme : Experience_names/parameters/")
 parser.add_argument("-e", "--n_epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("-b", "--batch_size", type=int, default=64, help="size of the batches")
