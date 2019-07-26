c62905b778e053456a3c32c045f844490e7f46a2
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-07-26 13:07:19 +0200

    testing the AEGEAN framework on meso

--- W17_AEGEAN/aegean.py
+++ W17_AEGEAN/aegean.py
@@ -1,8 +1,11 @@
 import AEGEAN as AG
-opt = AG.init()
 
+# VANILLA
+opt = AG.init()
 print(opt)
 AG.learn(opt)
 
+# what's the effect of a smaller latent_dim ? 
+opt = AG.init()
 opt.latent_dim, opt.runs_path = 4, 'small_latent'
 AG.learn(opt)
