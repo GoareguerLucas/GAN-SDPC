25c281f2b3b5554a81d739a0f19f5cfde37d6b2c
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-07-29 17:49:49 +0200

    exploiting AEGEAN framework: re-running

--- W17_AEGEAN/aegean.py
+++ W17_AEGEAN/aegean.py
@@ -1,11 +1,35 @@
+import os
 import AEGEAN as AG
 
 # VANILLA
+
 opt = AG.init()
 print(opt)
+#if os.path.isdir('runs/vanilla')
 AG.learn(opt)
 
-# what's the effect of a smaller latent_dim ? 
+# what's the effect of a smaller latent_dim ?
 opt = AG.init()
 opt.latent_dim, opt.runs_path = 4, 'small_latent'
+AG.learn(opt)
+
+# what's the effect of a smaller latent_dim ?
+opt = AG.init()
+opt.runs_path = 'small_eps'
+opt.bn_eps = 1e-10
+AG.learn(opt)
+
+opt = AG.init()
+opt.runs_path = 'big_eps'
+opt.bn_eps = 1e-2
+AG.learn(opt)
+
+opt = AG.init()
+opt.runs_path = 'small_momentum'
+opt.bn_momentum = .01
+AG.learn(opt)
+
+opt = AG.init()
+opt.runs_path = 'big_momentum'
+opt.bn_momentum = .5
 AG.learn(opt)
