e75ae294f0f3775da4d523358d0dec5eaa4b10ef
GoareguerLucas <lucas.goareguer@etu.univ-amu.fr>
2019-08-01 12:18:15 +0200

    Scan 4

---
+++ W18_AutoEncoder/scan_params4.py
@@ -0,0 +1,25 @@
+import sys
+
+sys.path.append("../")
+
+from utils import *
+
+"""
+Script à utiliser pour lancer un modèle avec différent paramètres à la suite, avec gestion des résultats dans tensorboard.
+Remplir la variables exp_name avec le nom de l'expérience et le dictionnaire params avec les paramètres à tester pour
+keys et leurs valeurs successive pour values.
+
+Pour scanner un nouveau paramètre il suffit d'ajouter l'argument correspondant dans sont dcgan.py. 
+Le script utiliser (dcgan.py) doit être compatible avec le paramètre --runs_path (-r) pour utiliser cette fonction 
+(cf. W13_current_dcgan/dcgan.py).
+Il ne faut pas ajouter au dictionnaire params l'argument --runs_path (-r), tout est gérer dans la fonction scan.
+"""
+
+exp_name = "AutoEncoder"
+
+# Suite de scan_params.py, recherche autour de lrD0.0001lrE0.001lrG1e05
+
+# Dictionnaire des paramètres à tester avec : noms du "paramètre" : [liste des valeurs]
+params = {"--b1": [0.5], "--eps": [0.0, 0.1], "--lrelu": [0.000001, 0.01], "--GPU": [1]}
+
+scan(exp_name, params)
