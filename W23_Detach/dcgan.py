5f9b9a15f0b12229925cfe7dae6ead4e3b83fe92
GoareguerLucas <lucas.goareguer@etu.univ-amu.fr>
2019-09-09 16:03:24 +0200

    BugFix

--- W23_Detach/dcgan.py
+++ W23_Detach/dcgan.py
@@ -23,7 +23,7 @@ import time
 import datetime
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-r", "--runs_path", type=str, default='AE_MAX/200e16i128b/',
+parser.add_argument("-r", "--runs_path", type=str, default='Detach/200e16i128b/',
                     help="Dossier de stockage des résultats sous la forme : Experience_names/parameters/")
 parser.add_argument("-e", "--n_epochs", type=int, default=300, help="number of epochs of training")
 parser.add_argument("-b", "--batch_size", type=int, default=16, help="size of the batches")
