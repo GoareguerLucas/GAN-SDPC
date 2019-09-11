8512aecf66f1c62089466d61c9e2d886200832af
GoareguerLucas <lucas.goareguer@etu.univ-amu.fr>
2019-09-11 15:10:52 +0200

    BugFix

--- W23_Interpol_SF/interpolation.py
+++ W23_Interpol_SF/interpolation.py
@@ -231,8 +231,8 @@ print(c.shape)
 noise = Variable(Tensor(c))
 sampling(noise, generator, opt.results_path, 0, tag=opt.tag, nrow=points)
 tensorboard_sampling(noise, generator, writer, 0, nrow=points, image_type="Interpolation A vers B")
-for i,line in enumerate(noise):
-	sampling(line, generator, opt.results_path, 0, tag=opt.tag+"_"+str(i), nrow=1)
+for i,line in enumerate(c):
+	sampling(Variable(Tensor(line)), generator, opt.results_path, 0, tag=opt.tag+"_"+str(i), nrow=1)
 
 # Analyse de l'espace
 print(diff)
