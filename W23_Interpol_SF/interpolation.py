86e11b7c076523a8dc11ff7051b2eb811595c4ca
GoareguerLucas <lucas.goareguer@etu.univ-amu.fr>
2019-09-11 12:07:04 +0200

    BugFix

--- W23_Interpol_SF/interpolation.py
+++ W23_Interpol_SF/interpolation.py
@@ -178,8 +178,8 @@ c = list()
 for i in range(N):
     c.append(np.linspace(a[i], b[i], points, endpoint=True))
 c = np.asarray(c)
-print(c.shape)
 print(c)
+print(c.shape)
 
 # Génération
 noise = Variable(Tensor(c))
