ca7acd3f06ff2dcc900e2c28b9a2a24c80ca6120
GoareguerLucas <lucas.goareguer@etu.univ-amu.fr>
2019-09-17 10:41:32 +0200

    Exp : WGAN

---
+++ W24_WGAN_SF/dcgan.py
@@ -0,0 +1,350 @@
+import argparse
+import os
+import numpy as np
+
+import itertools
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+from torch.utils.tensorboard import SummaryWriter
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+import sys
+
+import matplotlib.pyplot as plt
+import time
+import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--runs_path", type=str, default='WGAN/Scan1/',
+                    help="Dossier de stockage des résultats sous la forme : Experience_names/parameters/")
+parser.add_argument("-e", "--n_epochs", type=int, default=50, help="number of epochs of training")
+parser.add_argument("-b", "--batch_size", type=int, default=16, help="size of the batches")
+parser.add_argument("--lrD", type=float, default=0.00005, help="adam: learning rate for D")
+parser.add_argument("--lrG", type=float, default=0.00025, help="adam: learning rate for G")
+parser.add_argument("--eps", type=float, default=0.5, help="batchnorm: espilon for numerical stability")
+parser.add_argument("--lrelu", type=float, default=0.000001, help="LeakyReLU : alpha")
+parser.add_argument("--latent_dim", type=int, default=32, help="dimensionality of the latent space")
+parser.add_argument("-i", "--img_size", type=int, default=128, help="size of each image dimension")
+parser.add_argument("--channels", type=int, default=3, help="number of image channels")
+parser.add_argument("-s", "--sample_interval", type=int, default=5, help="interval between image sampling")
+parser.add_argument("--clip_value", type=float, default=0.01, help="lower and upper clip value for disc. weights")
+parser.add_argument("--sample_path", type=str, default='images')
+parser.add_argument("-m", "--model_save_interval", type=int, default=50,
+                    help="interval between model save. If model_save_interval > n_epochs : no save")
+parser.add_argument('--model_save_path', type=str, default='models')
+parser.add_argument('--load_model', action="store_true",
+                    help="Load model present in model_save_path/Last_*.pt, if present.")
+parser.add_argument("-d", "--depth", action="store_true",
+                    help="Utiliser si utils.py et SimpsonsDataset.py sont deux dossier au dessus.")
+parser.add_argument("-v", "--verbose", action="store_true",
+                    help="Afficher des informations complémentaire.")
+parser.add_argument("--GPU", type=int, default=0, help="Identifiant du GPU à utiliser.")
+opt = parser.parse_args()
+print(opt)
+
+# Particular import
+depth = ""
+if opt.depth == True:
+    depth = "../"
+sys.path.append(depth + "../")  # ../../GAN-SDPC/
+
+from SimpsonsDataset import SimpsonsDataset, FastSimpsonsDataset
+from utils import *
+from plot import *
+
+# Dossier de sauvegarde
+os.makedirs(opt.model_save_path, exist_ok=True)
+
+# Gestion du time tag
+try:
+    tag = datetime.datetime.now().isoformat(sep='_', timespec='seconds')
+except TypeError:
+    # Python 3.5 and below
+    # 'timespec' is an invalid keyword argument for this function
+    tag = datetime.datetime.now().replace(microsecond=0).isoformat(sep='_')
+tag = tag.replace(':','.')
+
+
+
+cuda = True if torch.cuda.is_available() else False
+NL = nn.LeakyReLU(opt.lrelu, inplace=True)
+opts_conv = dict(kernel_size=9, stride=2, padding=4, padding_mode='zeros')
+channels = [64, 128, 256, 512]
+
+class Generator(nn.Module):
+    def __init__(self, verbose=opt.verbose):
+        super(Generator, self).__init__()
+        
+        def generator_block(in_filters, out_filters):
+            block = [nn.UpsamplingNearest2d(scale_factor=opts_conv['stride']), nn.Conv2d(in_filters, out_filters, kernel_size=opts_conv['kernel_size'], stride=1, padding=opts_conv['padding'], padding_mode=opts_conv['padding_mode']), nn.BatchNorm2d(out_filters, opt.eps), NL]
+
+            return block
+
+        self.verbose = verbose
+        self.init_size = opt.img_size // opts_conv['stride']**3
+        self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, channels[3] * self.init_size ** 2), NL)
+
+
+        self.conv1 = nn.Sequential(*generator_block(channels[3], channels[2]),)
+        self.conv2 = nn.Sequential(*generator_block(channels[2], channels[1]),)
+        self.conv3 = nn.Sequential(*generator_block(channels[1], channels[0]),)
+        self.conv_blocks = nn.Sequential(
+            nn.Conv2d(channels[0], opt.channels, 3, stride=1, padding=1),
+            nn.Tanh(),
+        )
+        
+    def forward(self, z):
+        if self.verbose: print("G")
+        # Dim : opt.latent_dim
+        out = self.l1(z)
+        if self.verbose: print("l1 out : ",out.shape)
+        out = out.view(out.shape[0], channels[3], self.init_size, self.init_size)
+        # Dim : (channels[3], opt.img_size/8, opt.img_size/8)
+        if self.verbose: print("View out : ",out.shape)
+
+        out = self.conv1(out)
+        # Dim : (channels[3]/2, opt.img_size/4, opt.img_size/4)
+        if self.verbose: print("Conv1 out : ",out.shape)
+        out = self.conv2(out)
+        # Dim : (channels[3]/4, opt.img_size/2, opt.img_size/2)
+        if self.verbose: print("Conv2 out : ",out.shape)
+        out = self.conv3(out)
+        # Dim : (channels[3]/8, opt.img_size, opt.img_size)
+        if self.verbose: print("Conv3 out : ",out.shape)
+        
+        img = self.conv_blocks(out)
+        # Dim : (opt.chanels, opt.img_size, opt.img_size)
+        if self.verbose: print("img out : ", img.shape)
+
+        return img
+
+    def _name(self):
+        return "Generator"
+
+class Discriminator(nn.Module):
+    def __init__(self,verbose=opt.verbose):
+        super(Discriminator, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, bn=True):
+            block = [nn.Conv2d(in_filters, out_filters, **opts_conv), NL]#, nn.Dropout2d(0.25)
+            if bn:
+                block.append(nn.BatchNorm2d(out_filters, opt.eps))
+            return block
+
+        self.verbose = verbose
+
+        self.conv1 = nn.Sequential(*discriminator_block(opt.channels, channels[0], bn=False),)
+        self.conv2 = nn.Sequential(*discriminator_block(channels[0], channels[1]),)
+        self.conv3 = nn.Sequential(*discriminator_block(channels[1], channels[2]),)
+        self.conv4 = nn.Sequential(*discriminator_block(channels[2], channels[3]),)
+
+        # The height and width of downsampled image
+        self.init_size = opt.img_size // opts_conv['stride']**4
+        self.adv_layer = nn.Sequential(nn.Linear(channels[3] * self.init_size ** 2, 1))#, nn.Sigmoid()
+
+    def forward(self, img):
+        if self.verbose:
+            print("D")
+            print("Image shape : ",img.shape)
+            out = self.conv1(img)
+            print("Conv1 out : ",out.shape)
+            out = self.conv2(out)
+            print("Conv2 out : ",out.shape)
+            out = self.conv3(out)
+            print("Conv3 out : ",out.shape)
+            out = self.conv4(out)
+            print("Conv4 out : ",out.shape)
+
+            out = out.view(out.shape[0], -1)
+            print("View out : ",out.shape)
+            validity = self.adv_layer(out)
+            print("Val out : ",validity.shape)
+        else:
+            # Dim : (opt.chanels, opt.img_size, opt.img_size)
+            out = self.conv1(img)
+            # Dim : (channels[3]/8, opt.img_size/2, opt.img_size/2)
+            out = self.conv2(out)
+            # Dim : (channels[3]/4, opt.img_size/4, opt.img_size/4)
+            out = self.conv3(out)
+            # Dim : (channels[3]/2, opt.img_size/4, opt.img_size/4)
+            out = self.conv4(out)
+            # Dim : (channels[3], opt.img_size/8, opt.img_size/8)
+
+            out = out.view(out.shape[0], -1)
+            validity = self.adv_layer(out)
+            # Dim : (1)
+
+        return validity
+
+    def _name(self):
+        return "Discriminator"
+
+# Initialize generator and discriminator
+generator = Generator()
+discriminator = Discriminator()
+
+print_network(generator)
+print_network(discriminator)
+
+if cuda:
+    #print("Nombre de GPU : ",torch.cuda.device_count())
+    if torch.cuda.device_count() > opt.GPU: 
+        torch.cuda.set_device(opt.GPU)
+    
+    generator.cuda()
+    discriminator.cuda()
+
+# Initialize weights
+generator.apply(weights_init_normal)
+discriminator.apply(weights_init_normal)
+
+# Configure data loader
+dataloader = load_data(depth + "../../cropped_clear/cp/", opt.img_size, opt.batch_size, rand_hflip=True)
+
+# Optimizers
+optimizer_G = torch.optim.RMSprop(generator.parameters(), lr=opt.lrG)
+optimizer_D = torch.optim.RMSprop(discriminator.parameters(), lr=opt.lrD)
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+# ----------
+#  Load models
+# ----------
+start_epoch = 1
+if opt.load_model == True:
+    start_epoch = load_models(discriminator, optimizer_D, generator, optimizer_G, opt.n_epochs, opt.model_save_path)
+
+# ----------
+#  Tensorboard
+# ----------
+path_data1 = depth + "../runs/" + opt.runs_path
+path_data2 = depth + "../runs/" + opt.runs_path + tag[:-1] + "/"
+
+# Les runs sont sauvegarder dans un dossiers "runs" à la racine du projet, dans un sous dossiers opt.runs_path.
+os.makedirs(path_data1, exist_ok=True)
+os.makedirs(path_data2, exist_ok=True)
+
+writer = SummaryWriter(log_dir=path_data2)
+
+# ----------
+#  Training
+# ----------
+
+nb_batch = len(dataloader)
+nb_epochs = 1 + opt.n_epochs - start_epoch
+
+hist = init_hist(nb_epochs, nb_batch)
+
+save_dot = 1 # Nombre d'epochs avant de sauvegarder un point des courbes
+batch_on_save_dot = save_dot*len(dataloader)
+
+# Vecteur z fixe pour faire les samples
+N_samples = 24
+fixed_noise = Variable(Tensor(np.random.normal(0, 1, (N_samples, opt.latent_dim))))
+
+t_total = time.time()
+for j, epoch in enumerate(range(start_epoch, opt.n_epochs + 1)):
+    t_epoch = time.time()
+    for i, (imgs, _) in enumerate(dataloader):
+        t_batch = time.time()
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+        
+        # Configure input
+        real_imgs = Variable(imgs.type(Tensor))
+        # Generate a batch of images
+        z = np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))
+        z = Variable(Tensor(z))
+        gen_imgs = generator(z)
+
+        optimizer_D.zero_grad()
+
+        # Real batch
+        # Discriminator descision
+        d_x = discriminator(real_imgs)
+        # Fake batch
+        # Discriminator descision
+        d_g_z = discriminator(gen_imgs.detach())
+        
+        d_loss = -torch.mean(d_x) + torch.mean(d_g_z)
+        
+        d_loss.backward()
+        optimizer_D.step()
+
+        # Clip weights of discriminator
+        for p in discriminator.parameters():
+            p.data.clamp_(-opt.clip_value, opt.clip_value)
+
+        # -----------------
+        #  Train Generator
+        # -----------------
+
+        optimizer_G.zero_grad()
+
+        # New discriminator descision, Since we just updated D
+        d_g_z = discriminator(gen_imgs)
+        # Loss measures generator's ability to fool the discriminator
+        g_loss = -torch.mean(discriminator(gen_imgs))
+        # Backward
+        g_loss.backward()
+
+        optimizer_G.step()
+
+        print(
+            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f] [Time: %fs]"
+            % (epoch, opt.n_epochs, i+1, len(dataloader), d_loss.item(), g_loss.item(), time.time()-t_batch)
+        )
+
+        # Save Losses and scores for Tensorboard
+        save_hist_batch(hist, i, j, g_loss, d_loss, d_x, d_g_z)
+
+        # Tensorboard save
+        iteration = i + nb_batch * j
+        writer.add_scalar('g_loss', g_loss.item(), global_step=iteration)
+        writer.add_scalar('d_loss', d_loss.item(), global_step=iteration)
+
+        writer.add_scalar('d_x_mean', hist["d_x_mean"][i], global_step=iteration)
+        writer.add_scalar('d_g_z_mean', hist["d_g_z_mean"][i], global_step=iteration)
+
+        writer.add_scalar('d_x_cv', hist["d_x_cv"][i], global_step=iteration)
+        writer.add_scalar('d_g_z_cv', hist["d_g_z_cv"][i], global_step=iteration)
+
+        writer.add_histogram('D(x)', d_x, global_step=iteration)
+        writer.add_histogram('D(G(z))', d_g_z, global_step=iteration)
+
+    writer.add_scalar('D_x_max', hist["D_x_max"][j], global_step=epoch)
+    writer.add_scalar('D_x_min', hist["D_x_min"][j], global_step=epoch)
+    writer.add_scalar('D_G_z_min', hist["D_G_z_min"][j], global_step=epoch)
+    writer.add_scalar('D_G_z_max', hist["D_G_z_max"][j], global_step=epoch)
+
+    # Save samples
+    if epoch % opt.sample_interval == 0:
+        tensorboard_sampling(fixed_noise, generator, writer, epoch)
+
+    # Save models
+    if epoch % opt.model_save_interval == 0:
+        num = str(int(epoch / opt.model_save_interval))
+        save_model(discriminator, optimizer_D, epoch, opt.model_save_path + "/" + num + "_D.pt")
+        save_model(generator, optimizer_G, epoch, opt.model_save_path + "/" + num + "_G.pt")
+
+    print("[Epoch Time: ", time.time() - t_epoch, "s]")
+
+durer = time.gmtime(time.time() - t_total)
+print("[Total Time: ", durer.tm_mday - 1, "j:", time.strftime("%Hh:%Mm:%Ss", durer), "]", sep='')
+
+# Save model for futur training
+if opt.model_save_interval < opt.n_epochs + 1:
+    save_model(discriminator, optimizer_D, epoch, opt.model_save_path + "/last_D.pt")
+    save_model(generator, optimizer_G, epoch, opt.model_save_path + "/last_G.pt")
+
+writer.close()
---
+++ W24_WGAN_SF/scan_params.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.path.append("../")
+
+from utils import *
+
+"""
+Script à utiliser pour lancer un modèle avec différent paramètres à la suite, avec gestion des résultats dans tensorboard.
+Remplir la variables exp_name avec le nom de l'expérience et le dictionnaire params avec les paramètres à tester pour
+keys et leurs valeurs successive pour values.
+
+Pour scanner un nouveau paramètre il suffit d'ajouter l'argument correspondant dans sont dcgan.py. 
+Le script utiliser (dcgan.py) doit être compatible avec le paramètre --runs_path (-r) pour utiliser cette fonction 
+(cf. W13_current_dcgan/dcgan.py).
+Il ne faut pas ajouter au dictionnaire params l'argument --runs_path (-r), tout est gérer dans la fonction scan.
+"""
+
+exp_name = "WGAN/Scan1"
+
+# Dictionnaire des paramètres à tester avec : noms du "paramètre" : [liste des valeurs]
+params = {"--clip_value": [0.1, 0.01, 0.001]}
+
+scan(exp_name, params)
